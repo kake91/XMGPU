4dcb6e485469c2074c08b58d0e02b5a08c336a55
kake91 <148826970+kake91@users.noreply.github.com>
2023-10-23 21:02:06 -0400

    Update Miner2.sh

--- Miner2.sh
+++ Miner2.sh
@@ -65,6 +65,6 @@ tmux send-keys -t 0 'cd XENGPUMiner && python3 miner.py --gpu=true' C-m
 sleep 5
 echo "---------------------------"
 
-tmux send-keys -t 1 'cd XENGPUMiner && ./xengpuminer -d0 & ./xengpuminer -d1' C-m
+tmux send-keys -t 1 'sudo chmod +x xengpuminer  && ./xengpuminer -d0 & ./xengpuminer -d1' C-m
 sleep 5
 echo "---------------------------"
